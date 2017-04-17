4d8e3561f6168e8aa511fae38298ccb290679ea3
Jonathan Bober <jwbober@gmail.com>
2017-04-17 21:31:05 +0100

    long factorization

--- include/slint.h
+++ include/slint.h
@@ -648,18 +648,24 @@ static inline long ipow(long a, long n) {
     return z;
 }
 
-static std::vector<long> divisors(long n) {
-    std::vector<long> x;
-    for(int d = 1; d <= n; d++) {
-        if(n % d == 0) x.push_back(d);
-    }
-    return x;
-}
+//static std::vector<long> divisors(long n) {
+//    std::vector<long> x;
+//    for(int d = 1; d <= n; d++) {
+//        if(n % d == 0) x.push_back(d);
+//    }
+//    return x;
+//}
 
 struct int_factor_t {
     int p;
     int e;
     int f; // f == p^e
+};
+
+struct long_factor_t {
+    long p;
+    int e;
+    long f; // f == p^e
 };
 
 static int_factor_t * factor_table;
@@ -693,12 +699,55 @@ static void build_factor_table(int size) {
     factor_table_size = size;
 }
 
+
+static long_factor_t * long_factor_table;
+static long long_factor_table_size = 0;
+
+static void build_long_factor_table(long size) {
+    if(long_factor_table_size > 0) {
+        delete [] long_factor_table;
+    }
+    if(size == 0) {
+        long_factor_table_size = 0;
+        return;
+    }
+    long_factor_table = new long_factor_t[size];
+    for(long k = 0; k < size; k++) {
+        long_factor_table[k] = {1,1,1};
+    }
+    std::vector<long> primes;
+    prime_range(&primes, size);
+    for(auto p : primes) {
+        int e = 1;
+        long f = p;
+        while(f < size) {
+            for(long n = f; n < size; n += f) {
+                long_factor_table[n] = {p, e, f};
+            }
+            f = f * p;
+            e++;
+        }
+    }
+    factor_table_size = size;
+}
+
+
 struct int_factorization_t {
     // factorization of 32 bit (signed) integers
     int_factor_t factors[9];
     int n;
     int nfactors;
 };
+
+
+struct long_factorization_t {
+    // factorization of 64 bit (signed) integers
+    long_factor_t factors[16];
+    long n;
+    int nfactors;
+};
+
+
 
 static void factor(int n, int_factorization_t &factorization) {
     const int max_table_size = 100000000;
@@ -723,6 +772,32 @@ static void factor(int n, int_factorization_t &factorization) {
         factorization.nfactors++;
     }
 }
+
+static void factor_long(long n, long_factorization_t &factorization) {
+    const long max_table_size = 100000000;
+    long sign = 1;
+    if(n < 0) {sign = -1; n = -n;}
+    if(n >= factor_table_size) {
+        if(n < max_table_size) {
+            build_long_factor_table(std::max(100000l, std::min(2*n, max_table_size)));
+        }
+        else {
+            std::cerr << "Factorization of integers > " << max_table_size << " not currently supported." << std::endl;
+            std::cerr << "You might try changing the default max size in slint.h if you have lots of ram available." << std::endl;
+            exit(0);
+        }
+    }
+    factorization.n = n;
+    factorization.nfactors = 0;
+    while(n > 1) {
+        long_factor_t f = long_factor_table[n];
+        factorization.factors[factorization.nfactors] = f;
+        n = n/f.f;
+        factorization.nfactors++;
+    }
+}
+
+
 
 //std::vector<int> divisors(int n) {
 //    std::vector<int> x;
@@ -760,6 +835,39 @@ static std::vector<int> divisors(int n) {
 
     return x;
 }
+
+
+static std::vector<long> divisors(long n) {
+    std::vector<long> x;
+    long_factorization_t fac;
+    factor_long(n, fac);
+    int e[fac.nfactors];
+    for(int k = 0; k < fac.nfactors; k++) {e[k] = 0;}   // We'll write f = prod(fac.factors[k].p^e[k])
+    int f = 1;                                          // but we don't directly compute it this way.
+    do {
+        x.push_back(f);
+        int j = 0;
+        while(j < fac.nfactors && e[j] + 1 > fac.factors[j].e) {
+            while(e[j] > 0) {
+                e[j]--;
+                f = f/fac.factors[j].p;
+            }
+            j++;
+        }
+        if(j == fac.nfactors) f = 0;
+        else {
+            e[j]++;
+            f *= fac.factors[j].p;
+        }
+    } while(f != 0);
+
+    std::sort(x.begin(), x.end());
+
+    return x;
+}
+
+
+
 
 static int ndivisors(int n) {
     int_factorization_t fac;
